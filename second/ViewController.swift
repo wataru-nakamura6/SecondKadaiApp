38a53f45209212cc8792e336668bb514af975522
Wataru.Nakamura <puipui.sentinel@gmail.com>
2021-03-10 13:36:21 +0900

    完成

--- second/ViewController.swift
+++ second/ViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            // segueから遷移先のResultViewControllerを取得する
+        let View2:ViewController2 = segue.destination as! ViewController2
+        View2.name = TextField.text!
+    }
+        
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
     }
 
 
---
+++ second/ViewController2.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController2.swift
+//  second
+//
+//  Created by 中村航 on 2021/03/10.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var name:String = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        label.text = "ようこそ\(name)さん"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
