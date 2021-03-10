6e465a2711be234609132d170e6b321ac7715a72
Wataru.Nakamura <puipui.sentinel@gmail.com>
2021-03-10 12:58:13 +0900

    Initial Commit

---
+++ second/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  second
+//
+//  Created by 中村航 on 2021/03/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
